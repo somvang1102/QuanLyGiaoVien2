fc8201ef4df0015c1347f97b5c86200ff15df633
phamxuantien27 <65357044+phamxuantien27@users.noreply.github.com>
2020-12-22 18:13:48 +0700

    add form hoc sinh

---
+++ DTO/DTO_HocSinh.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_HocSinh
+    {
+        private string maHocSinh;
+        private string hoTen;
+        private DateTime ngaySinh;
+        private string email;
+    
+        private string gioiTinh;
+        private string lop;
+
+        public string MaHocSinh { get => maHocSinh; set => maHocSinh = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string Email { get => email; set => email = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string Lop { get => lop; set => lop = value; }
+    }
+
+}
