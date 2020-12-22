60e457d55eefec42479f676a68d0e2958f010ea1
phamxuantien27 <65357044+phamxuantien27@users.noreply.github.com>
2020-12-22 16:18:46 +0700

    add dang nhap

--- QuanLyGiaoVien/Form1.cs
+++ QuanLyGiaoVien/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLGV;
+using DTO;
 
 namespace QuanLyGiaoVien
 {
     public partial class Form1 : Form
     {
+        Bus_DangNhap dangNhap = new Bus_DangNhap();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dangNhap.DangNhap(txtTaiKhoan.Text, txtMatKhau.Text))
+            {
+                FormMain f = new FormMain();
+                this.Hide();
+                f.ShowDialog();
+                this.Close();
+                List<string> l = dangNhap.layTenND();
+            }
+            else
+            {
+                MessageBox.Show("Tên tài khoản hoặc mật khẩu không đúng");
+            }
+
         }
     }
 }
