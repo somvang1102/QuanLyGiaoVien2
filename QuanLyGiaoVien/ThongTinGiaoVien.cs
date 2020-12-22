4b4685e88a8b4ea5f0809abc6d77ba90a9f0e2fc
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-12-22 17:40:26 +0700

    add thong tin giao vien

--- QuanLyGiaoVien/ThongTinGiaoVien.cs
+++ QuanLyGiaoVien/ThongTinGiaoVien.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLGV;
+using DTO;
 
 namespace QuanLyGiaoVien
 {
     public partial class ThongTinGiaoVien : Form
     {
+        Bus_GiaoVien giaovien2 = new Bus_GiaoVien();
+        private string maGiaVien;
         public ThongTinGiaoVien()
         {
             InitializeComponent();
+        }
+
+        private void ThongTinGiaoVien_Load(object sender, EventArgs e)
+        {
+            DataTable dt2 = new DataTable();
+            dt2 = giaovien2.getGiaoVien(maGiaVien);
+           
+            txtmagv.Text = dt2.Rows[0]["HoTen"].ToString();
+            txtNS.Text = dt2.Rows[0]["NgaySinh"].ToString();
+            txtMaBM.Text = dt2.Rows[0]["TenBoMon"].ToString();
+            txtmaKhoa.Text = dt2.Rows[0]["TenKhoa"].ToString();
+            txtEmail.Text = dt2.Rows[0]["Email"].ToString();
+            txthoc.Text = dt2.Rows[0]["TenHocHam"].ToString();
+            txthocvi.Text = dt2.Rows[0]["TenHocVi"].ToString();
         }
     }
 }
